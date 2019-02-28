14204ab98801facef214bb1f3f19ebd214e3d136
Dave Parker <DYJParker@gmail.com>
2019-02-28 10:43:34 -0500

    Pre-circular reference commit

---
+++ app/src/main/java/tech/jpco/qen/model/MyUserRepository.kt
@@ -0,0 +1,26 @@
+package tech.jpco.qen.model
+
+import android.content.Context
+import io.reactivex.Observable
+import tech.jpco.qen.viewModel.DrawPoint
+import tech.jpco.qen.viewModel.MetaEvent
+import tech.jpco.qen.viewModel.MetaState
+
+class MyUserRepository private constructor(ctx: Context) : UserRepository {
+    companion object : SingletonHolder<MyUserRepository, Context>(::MyUserRepository)
+
+    private val sqlInstance = SQL.getInstance(ctx)
+
+    override fun addTouchStream(inStream: Observable<DrawPoint>): Observable<DrawPoint> =
+        inStream
+            .compose { sqlInstance.addTouchStream(it) }
+
+    override fun addMetaStream(inStream: Observable<MetaEvent>): Observable<MetaState> {
+        return sqlInstance.addMetaStream(inStream)
+    }
+}
+
+interface UserRepository {
+    fun addTouchStream(inStream: Observable<DrawPoint>): Observable<DrawPoint>
+    fun addMetaStream(inStream: Observable<MetaEvent>): Observable<MetaState>
+}
